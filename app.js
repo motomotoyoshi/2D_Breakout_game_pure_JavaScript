3f5b69def2f0c06a496d2cdee0e897b20e3e97b4
motomotoyoshi <motomoto.git@gmail.com>
2021-01-27 02:24:54 +0900

    Paddle_and_keyboard_controls

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
-
 const ballRadius = 10;
+
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+}
 
 function drawBall() {
   ctx.beginPath();
@@ -21,6 +35,7 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
 
   if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
@@ -30,7 +45,35 @@ function draw() {
     dy = -dy;
   }
 
+  if(rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
   x += dx;
   y += dy;
 }
+
+function keyDownHandler(e) {
+  if(e.key == 'Right' || e.key == 'ArrowRight') {
+    rightPressed = true;
+  }
+  if(e.key == 'Left' || e.key == 'ArrowLeft') {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if(e.key == 'Right' || e.key == 'ArrowRight') {
+    rightPressed = false;
+  }
+  if(e.key == 'Left' || e.key == 'ArrowLeft') {
+    leftPressed = false;
+  }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 setInterval(draw, 10);
