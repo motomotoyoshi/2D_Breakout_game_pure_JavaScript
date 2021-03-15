2cebc9626aea70f4cb185c9d8e05abf7bd3ea48b
motomotoyoshi <motomoto.git@gmail.com>
2021-03-16 01:19:24 +0900

    Add brick Row, Brick level

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let rightPressed = false;
 let leftPressed = false;
 
 // ブロックの変数
-let brickRowCount = 3;
+let brickRowCount = 4;
 let brickColumnCount = 5;
 let brickWidth = 75;
 let brickHeight = 20;
@@ -29,7 +29,7 @@ let bricks = [];
 for(let c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   for(let r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = {x: 0, y: 0, status: 1};
+    bricks[c][r] = {x: 0, y: 0, status: 3};
   }
 }
 
@@ -59,10 +59,10 @@ function collisionnDetection() {
   for(let c = 0; c < brickColumnCount; c++) {
     for(let r = 0; r < brickRowCount; r++) {
       let b = bricks[c][r];
-      if(b.status == 1) {
+      if(b.status > 0) {
         if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
           dy = -dy;
-          b.status = 0;
+          b.status--;
         }
       }
     }
@@ -88,14 +88,22 @@ function drawBall() {
 function drawBricks() {
   for(let c = 0; c < brickColumnCount; c++) {
     for(let r = 0; r < brickRowCount; r++) {
-      if(bricks[c][r].status == 1) {
+
+      if(bricks[c][r].status > 0) {
         let brickX = (c * (brickWidth + brickPadding)) + brickOffsetleft;
         let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
         bricks[c][r].x = brickX;
         bricks[c][r].y = brickY;
         ctx.beginPath();
         ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = "#0095DD";
+
+        if (bricks[c][r].status == 3) {
+          ctx.fillStyle = "#0095DD";
+        } else if (bricks[c][r].status == 2) {
+          ctx.fillStyle = "#DEB900";
+        } else {
+          ctx.fillStyle = "#DE0025";
+        }
         ctx.fill();
         ctx.closePath();
       }
@@ -110,26 +118,28 @@ function draw() {
   drawPaddle();
   collisionnDetection();
 
-  if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-    dx = -dx*1.03;
+  if(x + dx >= canvas.width - ballRadius || x + dx <= ballRadius) {
+    dx = -dx * 1.03;
   }
 
   if(y + dy < ballRadius) {
-    dy = -dy;
+    dy = -dy * 1.03;
   } else if (y + dy > canvas.height - ballRadius) {
     if (x > paddleX && x < paddleX + paddleWidth) {
-      dy = -dy*1.03;
+      if(y = y - paddleHeight ) {
+        dy = -dy * 1.03;
+      }
     } else {
-      clearInterval(interval); 
-      document.location.reload();
       alert("GAME OVER");
+      document.location.reload();
+      clearInterval(interval); 
     }
   }
 
   if(rightPressed && paddleX < canvas.width - paddleWidth) {
-    paddleX += 7;
+    paddleX += 6;
   } else if (leftPressed && paddleX > 0) {
-    paddleX -= 7;
+    paddleX -= 6;
   }
 
   x += dx*1.03;
