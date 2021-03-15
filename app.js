2fa043be711ce7a586547d2a7f1ef02ad331c139
motomotoyoshi <motomoto.git@gmail.com>
2021-03-15 23:57:18 +0900

    Collision_detection

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ let bricks = [];
 for(let c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   for(let r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = {x: 0, y: 0};
+    bricks[c][r] = {x: 0, y: 0, status: 1};
   }
 }
 
@@ -55,6 +55,20 @@ function keyUpHandler(e) {
   }
 }
 
+function collisionnDetection() {
+  for(let c = 0; c < brickColumnCount; c++) {
+    for(let r = 0; r < brickRowCount; r++) {
+      let b = bricks[c][r];
+      if(b.status == 1) {
+        if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy;
+          b.status = 0;
+        }
+      }
+    }
+  }
+}
+
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
@@ -74,15 +88,17 @@ function drawBall() {
 function drawBricks() {
   for(let c = 0; c < brickColumnCount; c++) {
     for(let r = 0; r < brickRowCount; r++) {
-      let brickX = (c * (brickWidth + brickPadding)) + brickOffsetleft;
-      let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+      if(bricks[c][r].status == 1) {
+        let brickX = (c * (brickWidth + brickPadding)) + brickOffsetleft;
+        let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
@@ -92,6 +108,7 @@ function draw() {
   drawBricks();
   drawBall();
   drawPaddle();
+  collisionnDetection();
 
   if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx*1.03;
